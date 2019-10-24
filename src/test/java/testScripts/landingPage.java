2b776ec4b704520630a379b2b8abfb5695aa01e9
Mallikarjun S <mallusalimath203@gmail.com>
2019-10-24 16:49:53 +0530

    Initial commit

---
+++ src/test/java/testScripts/landingPage.java
@@ -0,0 +1,13 @@
+package testScripts;
+
+import org.testng.annotations.Test;
+
+public class landingPage
+{
+    @Test
+    void print ()
+    {
+
+    }
+
+}
---
+++ src/test/java/testScripts/listeners.java
@@ -0,0 +1,44 @@
+package testScripts;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import resources.base;
+
+public class listeners implements ITestListener
+{
+    base b = new base();
+
+    public void onTestStart(ITestResult result) {
+
+    }
+
+    public void onTestSuccess(ITestResult result) {
+
+    }
+
+    public void onTestFailure(ITestResult result) {
+
+    }
+
+    public void onTestSkipped(ITestResult result) {
+
+    }
+
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+
+    }
+
+    public void onTestFailedWithTimeout(ITestResult result) {
+
+    }
+
+    public void onStart(ITestContext context) {
+
+    }
+
+    public void onFinish(ITestContext context) {
+
+    }
+
+}
